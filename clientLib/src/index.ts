ffe5c67feaad1d0df3eb5d1e95286ee7cf07e45c
Gustavo Carvalho <gustavo.carvalho@dao.cg>
2025-02-19 21:06:09 +0000

    Properly export types

--- clientLib/src/index.ts
+++ clientLib/src/index.ts
@@ -1,1 +1,2 @@
 export { default as CgPluginLib } from './cgPluginLib';
+export { type CommunityInfoResponsePayload, type UserInfoResponsePayload, type ActionResponsePayload, type AnyResponsePayload } from './types';
